11558670128b430ca890d9ce48f67c6aa0a3bd2d
showvike <showvike@gmail.com>
2023-05-20 08:56:18 +0600

    single toy api done

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 
 const app = express();
@@ -52,6 +52,13 @@ async function run() {
 
       const cursor = toysCollection.find(query, options).limit(lt);
       const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.get("/toy/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await toysCollection.findOne(query);
       res.send(result);
     });
 
